c419559c1e22e39d0013030fc2ef3c90b02fd31d
Joseph <joseph.dena@student.moringaschool.com>
2023-10-25 16:19:12 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+
+// Remove the existing 'main' element if it exists
+const existingMain = document.querySelector("main");
+if (existingMain) {
+  existingMain.remove();
+}
+
+// Create a new <h1> element with the id 'victory'
+const newHeader = document.createElement("h1");
+newHeader.id = "#victory";
+newHeader.textContent = "victory is the champion";
+
+// Append the new <h1> element to the document body
+document.body.appendChild(newHeader);
+
+
